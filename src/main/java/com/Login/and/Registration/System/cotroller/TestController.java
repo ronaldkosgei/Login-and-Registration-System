637889b4a54e2feb72164f9c62559ef47b2ff83d
radian56 <82970578+radian56@users.noreply.github.com>
2023-04-28 14:03:51 +0300

    project-setup

--- src/main/java/com/Login/and/Registration/System/cotroller/TestController.java
+++ src/main/java/com/Login/and/Registration/System/cotroller/TestController.java
@@ -1,2 +1,14 @@
-package com.Login.and.Registration.System.cotroller;public class TestController {
+package com.Login.and.Registration.System.cotroller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("sample")
+public class TestController {
+
+ @GetMapping("run")
+ public String getSample(){
+     return "Hello";
+ }
 }
