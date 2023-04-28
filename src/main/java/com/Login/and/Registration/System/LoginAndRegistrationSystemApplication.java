adce8df8fde1ad792167ed82f81360691cfd30dc
radian56 <82970578+radian56@users.noreply.github.com>
2023-04-28 11:33:10 +0300

    project setup

---
+++ src/main/java/com/Login/and/Registration/System/LoginAndRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Login.and.Registration.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Login/and/Registration/System/LoginAndRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Login.and.Registration.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
